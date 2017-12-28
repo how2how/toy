fe78b9de70ae74bd0c121911b1de5db4f8143de5
test <test@test.com>
2017-12-29 00:40:43 +0800

    test importer

--- modules/listdir.py
+++ modules/listdir.py
@@ -5,10 +5,10 @@
 # @Last Modified time: 2017-01-19 12:47:24
 
 import os
-import packages.test as info
+from packages.test import run as info
 
 
 def run(**args):
     print '[*] In listdir module.'
     files = os.listdir('.')
-    return info.run() + '\n' + str(files)
+    return info() + '\n' + str(files)
