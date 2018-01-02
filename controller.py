74a78de780358050f93483abf7a893819872fb29
arp <arp@gitosc.com>
2018-01-01 19:32:44 -0800

    test importer

---
+++ controller.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Author: arp
+# @Date:   2017-12-27 19:09:16
+# @Last Modified by:   uuu
+# @Last Modified time: 2017-12-27 19:09:32
--- modules/listdir.py
+++ modules/listdir.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 # @Author: test
 # @Date:   2017-01-19 12:44:34
-# @Last Modified by:   test
-# @Last Modified time: 2017-01-19 12:47:24
+# @Last Modified by:   arp
+# @Last Modified time: 2018-01-01 19:20:50
 
 import os
-from packages import test as info
+from packages.test import run as info
 
 
 def run(**args):
     print '[*] In listdir module.'
     files = os.listdir('.')
-    return info.run() + '\n' + str(files)
+    return info() + '\n' + str(files)
