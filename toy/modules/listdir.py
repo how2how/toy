9bae225668b39433ba4ae474d542e6fc1f2e984b
test <test@test.com>
2018-01-04 22:56:44 +0800

    change modules import

--- toy/modules/listdir.py
+++ toy/modules/listdir.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 # @Author: test
 # @Date:   2017-01-19 12:44:34
-# @Last Modified by:   arp
-# @Last Modified time: 2018-01-01 19:20:50
+# @Last Modified by:   test
+# @Last Modified time: 2018-01-04 22:55:19
 
 import os
-from packages.test import run as info
+from toy.modules.packages import test as info
 
 
 def run(**args):
     print '[*] In listdir module.'
     files = os.listdir('.')
-    return info() + '\n' + str(files)
+    return info.run() + '\n' + str(files)
