70fe62a753490924e0bb7a24a8d574d3ed1813ba
test <test@test.com>
2018-01-11 11:42:22 +0800

    test

--- toy/boy.py
+++ toy/boy.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import sys
+sys.path.remove('')
 import json
 import time
 import base64
--- toy/miniloader.py
+++ toy/miniloader.py
@@ -4,7 +4,9 @@ add_remote_repo(
     ['toy'],
     'https://raw.githubusercontent.com/how2how/toy/master')
 from toy.boy import Boy
+import json
 
-
-config = Boy.get_config(Boy.config_url + Boy._id + '.json')
+# config = Boy.get_config(Boy.config_url + Boy._id + '.json')
+config = json.load(open('../test_conf.json','rb'))
 b = Boy(config)
+b.run()
